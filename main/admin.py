0481006ab5281008c914a79cfecf5588fba419f8
prakhar1144 <prakhar841301@gmail.com>
2020-10-21 20:36:40 +0530

    Finance db setup

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Faculty_db,Student_db
+from .models import Faculty_db,Student_db,Finance_db
 
 # Register your models here.
 
 admin.site.register(Faculty_db)
 admin.site.register(Student_db)
+admin.site.register(Finance_db)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Finance_db(models.Model):
+	Joined = models.DateField()
+	Paid_Till = models.DateField()
+
+
+
 class Faculty_db(models.Model):
 	Name = models.CharField(max_length=30)
 	Subject = models.CharField(max_length=30)
@@ -8,6 +14,8 @@ class Faculty_db(models.Model):
 	Email = models.EmailField(max_length=254)
 	#Payment
 	#Attendance
+	Payment = models.ForeignKey(Finance_db, on_delete=models.PROTECT, default=None)
+
 
 	def __str__(self):
 		return self.Name
@@ -18,6 +26,7 @@ class Student_db(models.Model):
 
 	
 	Name = models.CharField(max_length=30)
+	Roll_Number = models.DecimalField(max_digits=10, decimal_places=0)
 	BirthDate = models.DateField(null=True, blank=True)
 	Class =  models.CharField(max_length=10, choices= options, default = 'First')
 	#Result
@@ -25,10 +34,13 @@ class Student_db(models.Model):
 	Email = models.EmailField(max_length=254, null= True, blank= True)
 	#Fee
 	#Attendance
+	Fee = models.ForeignKey(Finance_db, on_delete=models.PROTECT, default=None)
 
 	class Meta:
 		ordering = ['Class']
 
 	def __str__(self):
 		return self.Name
+
+
 
